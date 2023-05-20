cc6936cd3312780f9283cd22d581980466b6e050
vtr-delfino <91512605+vtr-delfino@users.noreply.github.com>
2023-05-20 00:09:03 -0300

    Organise CSS and did some polish, add gtp buttom

--- js/index.js
+++ js/index.js
@@ -1,14 +1,46 @@
-hambMenu = document.getElementById("menuToggle");
-hambMenuCheckbox = document.getElementById("hambMenuCheckbox");
+$hambMenu = document.getElementById("menu")
+$hambMenuToggle = document.getElementById("menuToggle")
+$hambMenuCheckbox = document.getElementById("hambMenuCheckbox")
 
-let hideOnScroll = function() {
-  let y = window.scrollY;
-  if (y >= 38) {
-    hambMenu.className = "show"
-  } else {
-    hambMenu.className = "hide"
-    hambMenuCheckbox.checked = false;
+$btnGetToTop = document.getElementById("getToTop")
+
+let hambMenuScroll = function() 
+{
+  let y = window.scrollY
+  
+  if (y > 37) 
+  {
+    $hambMenuToggle.classList.remove("hide")
+    $hambMenuToggle.classList.add("show")
+
+    $hambMenu.classList.remove("hide")
+    $hambMenu.classList.add("show")
+  } 
+  else 
+  {
+    $hambMenuToggle.classList.remove("show")
+    $hambMenuToggle.classList.add("hide")
+
+    $hambMenu.classList.remove("show")
+    $hambMenu.classList.add("hide")
+
+    $hambMenuCheckbox.checked = false
   }
-};
+}
 
-window.addEventListener("scroll", hideOnScroll);
+let btnGetToTopScroll = function() 
+{
+  let y = window.scrollY
+
+  if (y > 900) 
+  {
+    $btnGetToTop.classList.remove("hide")
+    $btnGetToTop.classList.add("show")
+  } else {
+    $btnGetToTop.classList.remove("show")
+    $btnGetToTop.classList.add("hide")
+  }
+}
+
+window.addEventListener("scroll", hambMenuScroll)
+window.addEventListener("scroll", btnGetToTopScroll)
